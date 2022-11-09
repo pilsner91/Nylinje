b359ed614ab96a35576a72aef71170523cb5f351
toften05 <jacob.toft2001@gmail.com>
2022-11-09 13:38:20 +0100

    idk

---
+++ DB/src/main/java/IMPL/ItemServiceImpl.java
@@ -0,0 +1,26 @@
+package IMPL;
+
+import GRPC.item.*;
+import GRPC.shelf.SearchShelfParametersProto;
+import GRPC.shelf.ShelfProto;
+import GRPC.shelf.ShelvesGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class ItemServiceImpl extends ItemsGrpc.ItemsImplBase {
+/*
+    public void getItem(SearchItemsParametersProto itemRequest, StreamObserver<ItemProto> itemResponds){
+        ItemProto itemPrto = ItemProto.newBuilder().setId(2).setShelfId(2).setu
+    }
+
+    public void getShelf(SearchShelfParametersProto request, StreamObserver<ShelfProto> response
+    ){
+        ShelfProto fisk = ShelfProto.newBuilder().setShelfNo("2").setDimensionX(2).setDimensionY(2).setDimensionZ(2).setRowNo("3").build();
+
+    }
+
+
+
+ */
+
+
+}
