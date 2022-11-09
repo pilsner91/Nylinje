6a9cd15cd6353f138c6178deadfbd7e3a1874ef9
mpede <mpeder75@gmail.com>
2022-11-09 12:29:24 +0100

    Projekt rename, da Mikkel pc er lårt

---
+++ DB/src/test/java/me/via316455/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package me.via316455.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
