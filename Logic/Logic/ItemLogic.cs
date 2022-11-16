d2288d7dd426d4e6689c022fab3e7bb47dca6c48
mpede <mpeder75@gmail.com>
2022-11-16 11:54:44 +0100

    Test for connection

--- Logic/Logic/ItemLogic.cs
+++ Logic/Logic/ItemLogic.cs
@@ -5,7 +5,7 @@ using Shared.Model;
 namespace Logic.Logic; 
 
 public class ItemLogic : IItemLogic {
-    public async Task<Shared.Model.Item> CreateAsync(ItemCreationDto dto) {
+    public async Task<Shared.Model.Item>  CreateAsync(ItemCreationDto dto) {
         throw new NotImplementedException();
     }
 }
