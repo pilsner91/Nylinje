92dec7ed1e06020c92071794b460050db3c6a5c2
morte <morten_oel@hotmail.com>
2022-11-23 12:19:16 +0100

    HTTPClient Begin

--- BlazerWASM/Program.cs
+++ BlazerWASM/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IItemService, ItemHttpClient>();
+builder.Services.AddScoped<IItemTypeService, ItemTypeHttpClient>();
 
 builder.Services.AddScoped(
     sp => 
---
+++ HttpClients/ClientInterfaces/IShelfService.cs
@@ -0,0 +1,8 @@
+﻿using Shared.DTOs;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IShelfService
+{
+    Task<bool> AddItemToShelf(List<ShelfAddItemRequestDto> dto);
+}
