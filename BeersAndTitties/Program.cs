47d29c636432bfaed7b37bd12811df3fc9b8cca5
toften05 <jacob.toft2001@gmail.com>
2022-11-23 10:43:54 +0100

    ny protofil

--- BeersAndTitties/Program.cs
+++ BeersAndTitties/Program.cs
@@ -1,20 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Grpc.Net.Client;
-using GRPC.Shelf;
+using GRPC.Item;
+
+
+
+
 
 SearchShelfParametersProto shelfParametersProto = new SearchShelfParametersProto {Id = 1};
+SearchItemsParametersProto itemsParametersProto = new SearchItemsParametersProto { Id = 1 };
+
 
 using var channel = GrpcChannel.ForAddress("http://localhost:9090");
-var client = new Shelves.ShelvesClient(channel);
+var shelvesClient = new Shelves.ShelvesClient(channel);
+var itemClient = new Items.ItemsClient(channel);
+
+
+
 
 
 async Task<ShelfProto> GetShelf(SearchShelfParametersProto search)
 {
-    ShelfProto shelfProto = await client.GetShelfAsync(search);
+    ShelfProto shelfProto = await shelvesClient.GetShelfAsync(search);
     return shelfProto;
 }
-
 ShelfProto shelfProto = await GetShelf(shelfParametersProto);
+
+
+async Task<ItemProto> GetItem(SearchItemsParametersProto search)
+{
+    ItemProto itemProto = await itemClient.GetItemsAsync(search);
+    return itemProto;
+}
+
+ItemProto itemProto = await GetItem(itemsParametersProto);
+
+
+
+
 Console.WriteLine(shelfProto.RowNo);
-Console.WriteLine("hello");
+Console.WriteLine(itemProto.Id);
+
+
