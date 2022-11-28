d420d24ac0d610b3fb697339fc003947e000f4a2
toften05 <jacob.toft2001@gmail.com>
2022-11-28 11:54:31 +0100

    ShelfPage.razor er blevet opdaret, men mangler stadig visse funktionalitetter

---
+++ Domain/DTOs/ShelfSpaceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DTOs;
+
+public class ShelfSpaceDTO
+{
+    public int? ShelfID { get; set; }
+    public int? AvalibleSpace { get; set; }
+    public int? InputWished { get; set; }
+}
