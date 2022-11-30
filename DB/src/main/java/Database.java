cb77a3ce48e57a7776f1b3a3dce56cfc961df7c6
morte <morten_oel@hotmail.com>
2022-11-30 11:30:10 +0100

    Spring grpc

--- DB/src/main/java/Database.java
+++ DB/src/main/java/Database.java
@@ -1,9 +1,11 @@
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class Database {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server = ServerBuilder
