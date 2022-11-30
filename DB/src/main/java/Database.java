d928e2bd0b32381224e81464b4fd21b02988931e
morte <morten_oel@hotmail.com>
2022-11-30 13:05:47 +0100

    Startet med grpc modtager server

--- DB/src/main/java/Database.java
+++ DB/src/main/java/Database.java
@@ -1,6 +1,7 @@
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -8,14 +9,9 @@ import java.io.IOException;
 @SpringBootApplication
 public class Database {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder
-                .forPort(9090)
-                //.addService(new ShelfServiceIMPL())
-                //.addService(new ItemServiceImpl())
-                .build();
+        SpringApplication.run(Database.class,args);
 
-        server.start();
-        server.awaitTermination();
+
     }
 
 }
