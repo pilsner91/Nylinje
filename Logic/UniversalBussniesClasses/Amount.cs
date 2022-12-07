274f5b1dabace6dda5e64e135d334e9f86b73f0a
morte <morten_oel@hotmail.com>
2022-12-07 09:11:07 +0100

    Made so it the items reduces the volume left in a shelf

--- Logic/UniversalBussniesClasses/Amount.cs
+++ Logic/UniversalBussniesClasses/Amount.cs
@@ -10,6 +10,11 @@ public class Amount
         double volumenShelf = shelf.DimX * shelf.DimY * shelf.DimZ;
         double volumenItemType = _itemType.DimX * _itemType.DimY * _itemType.DimZ;
 
+        foreach (Item item in shelf.ItemsOnShelf)
+        {
+            volumenShelf -= item.Type.DimX * item.Type.DimY * item.Type.DimZ;
+        }
+
         return new AmountOnSpaceDto(shelf.RowNo+shelf.ShelfNo,(int?)Math.Floor(volumenShelf / volumenItemType));
     }
 }
